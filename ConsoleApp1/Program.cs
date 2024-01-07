cf90554f685b460fb78f45cd6bfbd8b436ba8f5a
Jakub Pacler <jakub.pacler@gmail.com>
2024-01-07 12:43:52 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program sprawdza ilość samogłosek we wprowadzonym słowie.");
+            Console.Write("Podaj słowo do sprawdzdenia ilości samogłosek: ");
+            string slowo = Console.ReadLine();
+
+            Console.WriteLine(IloscSamoglosek(slowo));
+
+            Console.ReadKey();
+
+        }
+
+        static int IloscSamoglosek(string slowo)
+        {
+            int ilosc = 0;
+            slowo = slowo.ToLower();
+            foreach (char litera in slowo)
+            {
+                if ("aeiouy".Contains(litera))
+                {
+                    ilosc++;
+                }
+            }
+            return ilosc;
+        }
+
+    }
+}
